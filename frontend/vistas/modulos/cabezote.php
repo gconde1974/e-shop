7f50e4880f641b70e6c63a30ee83af04da40e8a2
gconde1974 <gabriel@gconde.com>
2018-09-10 16:14:45 -0500

    Initial commit

---
+++ frontend/vistas/modulos/cabezote.php
@@ -0,0 +1,127 @@
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- redes sociales -->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul>
+                    <li>
+                        <a href="http://facebook.com" target="_blank">
+                            <i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://youtube.com" target="_blank">
+                            <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://twitter.com" target="_blank">
+                            <i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://google.com" target="_blank">
+                            <i class="fa fa-google redSocial googleBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://instagram.com" target="_blank">
+                            <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 social">
+                <ul>
+                    <li><a href="#modalIngreso" data-toggle="modal">Ingreso</a></li>
+                    <li>|</li>
+                    <li><a href="#modalregistro" data-toggle="modal">Crear una cuenta</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<header class="container-fluid">
+    <div class="container">
+        <div class="row" id="cabezote"> 
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+                <a href="#">
+                    <img src="http://localhost/e-shop/backend/vistas/img/plantilla/logo.png" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+                    <p> CATEGORIAS
+                        <span class="pull-right"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                    </p>                    
+                </div>
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+                    <input type="search" name="buscador" class="form-control" placeholder="Buscar...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+                <a href="#">
+                    <button class="btn btn-default pull-left backColor">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>Tu cesta <span class="cantidadCesta"></span><br> USD $ <span class="sumaCesta"></span></p>
+            </div>
+        </div>
+        <div class="col-xs-12 backColor" id="categorias">
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4><a href="#" class="pixelCategorias">Lorem ipsum</a></h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem ipsum</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tienda Virtual</title>
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plantilla.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet"> 
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+    // HEADER
+    include "modulos/cabezote.php";
+    ?>
+</body>
+</html>
